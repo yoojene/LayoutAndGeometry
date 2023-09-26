285f103c81f5a21e474c767a89848f56f2fc437a
Eugene Cross <eugene.cross@gmail.com>
2023-09-26 17:11:18 +0100

    Day 91: changing alignments, tear effect

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -11,12 +11,12 @@ struct ContentView: View {
     var body: some View {
      
         VStack(alignment: .leading) {
-            Text("Hello, world")
-                .alignmentGuide(.leading) { d in
-                    d[.trailing]
-                }
-//                .offset(x: -100, y: 0)
-            Text("This is a longer line of text")
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { d in
+                        Double(position) * -10
+                    }
+            }
         }
         .background(.red)
         .frame(width: 400, height: 400)
