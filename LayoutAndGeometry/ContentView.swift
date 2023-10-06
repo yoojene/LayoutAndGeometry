b4c3eab19e4f4f77928ec79a5bda5e8ba082c245
Eugene Cross <eugene.cross@gmail.com>
2023-10-06 22:49:27 +0100

    Day 93 ScrollEffects using geometry reader, multiple geometry readers pt2

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -14,18 +14,21 @@ struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     
     var body: some View {
-        ScrollView {
-            ForEach(0..<50) { index in
-                GeometryReader { geo in
-                    Text("Row #\(index)")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                        .background(colors[index % 7])
-                        .rotation3DEffect(.degrees(geo.frame(in: .global).minY / 5), axis: (x: 0, y: 1, z: 0))
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
                 }
-                .frame(height: 40)
             }
         }
+
     }
 }
 
