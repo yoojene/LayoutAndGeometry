8b20aa337fca3c1e40a96cb524684645ff687b14
Eugene Cross <eugene.cross@gmail.com>
2023-10-06 22:34:07 +0100

    Day 93 Understanding frames and coordinates inside GeometryReader Pt2

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -26,35 +26,24 @@ struct InnerView: View {
                 Text("Center")
                     .background(.blue)
                     .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x  \(geo.frame(in: .global).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x  \(geo.frame(in: .local).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x  \(geo.frame(in: .named("Custom")).midY)")
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
 
                     }
                 
             }
             .background(.orange)
-                
+            Text("Right")
         }
     }
 }
 struct ContentView: View {
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                Text("Hello. world!")
-                    .frame(width: geo.size.width * 0.9)
-                    .background(.red)
-            }
-            .background(.green)
-            Text("More text")
-            Text("More text")
-            Text("More text")
-            Text("More text")
-            Text("More text")
-                .background(.blue)
-        }
-        
+
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name:"Custom")
        
     }
 }
