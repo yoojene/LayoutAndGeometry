9c838414dfa0a79bcb829ee300ffff0a08caa9a2
Eugene Cross <eugene.cross@gmail.com>
2023-10-06 22:13:10 +0100

    Day 93 - offset and modifiers - absolute positioning

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,39 +8,12 @@
 import SwiftUI
 
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID { // set as a enum rather than a struct as making an instance of this type does not make sense
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-        
-        
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self) // helper const
-}
-
 
 struct ContentView: View {
     var body: some View {
-     
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@yoojene")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center]}
-                Image("lakeland")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("EUGENE CROSS")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center]}
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello. world!")
+            .offset(x: 100, y: 100) // offset is not changing position of Text, merely rendering it elsewhere
+            .background(.red) // background still working on original position
     }
 }
 
