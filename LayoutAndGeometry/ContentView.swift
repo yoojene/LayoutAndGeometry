539c0641a3e6ac1dce50bc42fbfc537ebb193784
Eugene Cross <eugene.cross@gmail.com>
2023-10-07 14:01:19 +0100

    Day 94 - Challenge 3, change colors based on position

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -12,20 +12,18 @@ import SwiftUI
 struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
 
-    
-    @State private var fadeOutOpacity = 1.0
-    
-    
+
     var body: some View {
         GeometryReader { fullView in
             ScrollView(.vertical) {
                 ForEach(0..<50) { index in
                     GeometryReader { geo in
+                        Text("Min y #\(geo.frame(in: .global).minY * 0.001)")
                         Text("Row #\(index)")
                             .font(.title)
                             .frame(maxWidth: .infinity)
                             .frame(height: abs(geo.frame(in: .global).midY / 20.0))
-                            .background(colors[index % 7])
+                            .background(Color(hue: geo.frame(in: .global).minY * 0.001, saturation: geo.frame(in: .global).minY * 0.002, brightness: geo.frame(in: .global).minY * 0.003))
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             .opacity(geo.frame(in: .global).minY * 0.01 - 1.0 / 2.0)
                             
